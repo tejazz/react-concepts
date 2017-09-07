962da0f64b27540a29a7be5b8dbdd799eb4ec92a
Tarique Ejaz <M1039130@mindtree.com>
2017-09-07 18:59:49 +0530

    Changes in license file of boilerplate template

---
+++ weather-forecast-app/src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'd8cddf31f954f48c68eb51ec1b1b617a'; 
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
